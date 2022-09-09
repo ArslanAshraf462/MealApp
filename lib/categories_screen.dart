671d377a1e5e4ea6cda80068b94a852a11c922c8
ArslanAshraf462 <arslan.ashraf.oraxtech@gmail.com>
2022-09-09 18:39:12 +0500

    Creating Grid & using Linear Gradient & styling & theming

---
+++ lib/categories_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+import 'package:meals_app/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(25.0),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200.0,
+        childAspectRatio: 3/2,
+        crossAxisSpacing: 20.0,
+        mainAxisSpacing: 20.0,
+        ),
+      children: DUMMY_CATEGORIES.map((catData) {
+        return CategoryItem(
+          catData.title, 
+          catData.color);
+      }).toList(),
+      ),);
+  }
+}
